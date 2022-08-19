c13be87d8849175e6b4dbe2e34a5924f761b0450
Vanessa Furtunato <vanessafurtunato984@gmail.com>
2022-08-18 23:16:37 -0300

    class 6

---
+++ module-one/class06a.py
@@ -0,0 +1,5 @@
+number1 = int(input('Type a value: '))
+number2 = int(input('Type another value: '))
+total = number1 + number2
+print('The total between {} and {} is {}!'.format(number1, number2, total))
+
