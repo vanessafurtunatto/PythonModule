f8a062a1b6612b27e12a96d99ae2a3b23da7d13c
Vanessa Furtunato <vanessafurtunato984@gmail.com>
2022-08-23 19:20:49 -0300

    Predecessor and Successor

---
+++ module-one/ex006.py
@@ -0,0 +1,3 @@
+print('Exercise about predecessor and successor')
+num = int(input('Type value: '))
+print('The value selected is {}, your predecessor is {} and successor is {}'.format(num, (num-1), (num+1)))
