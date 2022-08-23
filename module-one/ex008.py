ab62ece9fe6b361561a1db9028754172bf23af1b
Vanessa Furtunato <vanessafurtunato984@gmail.com>
2022-08-23 19:48:43 -0300

    Average

---
+++ module-one/ex008.py
@@ -0,0 +1,5 @@
+print('Exercise about average')
+score1 = float(input('Type first score: '))
+score2 = float(input('Type second score: '))
+av = (score1 + score2) / 2
+print('Your average is: {}'.format(av))
