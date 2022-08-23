a34aad797cf1efbff9939fe4f80b222a0176ea1f
Vanessa Furtunato <vanessafurtunato984@gmail.com>
2022-08-23 19:37:31 -0300

    Double, triple and square root

---
+++ module-one/ex007.py
@@ -0,0 +1,4 @@
+print('Exercise about double, triple and square root ')
+num = int(input('Type value: '))
+print('The value selected is {}, \n Your double is {}, \n Your triple is {}, \n And your square root is {}.9'
+      .format(num, (num*2), (num*3), (num**(1/2))))
