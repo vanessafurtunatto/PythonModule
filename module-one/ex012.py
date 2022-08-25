3ef3abc6de86be100fe34e6bf7630ff3f61b9b80
Vanessa Furtunato <vanessafurtunato984@gmail.com>
2022-08-24 21:00:52 -0300

    Painting the wall

---
+++ module-one/ex012.py
@@ -0,0 +1,7 @@
+print('Painting the wall')
+al = float(input('How tall? '))
+la = float(input('How wide? '))
+ar = al * la
+print('Your wall has a height of {} x {}  and its area is {}'.format(al, la, ar))
+paint = ar / 2
+print('You will need {} of paint'.format(paint))
