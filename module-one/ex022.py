0dc87774d2351caed73b0822736d5fb0da8d74de
Vanessa Furtunato <vanessafurtunato984@gmail.com>
2022-08-30 21:42:39 -0300

    Sine, Cosine, and Tangent

---
+++ module-one/ex022.py
@@ -0,0 +1,6 @@
+from math import radians, sin, cos, tan
+angle = float(input(''))
+sen = sin(radians(angle))
+cos = cos(radians(angle))
+tan = tan(radians(angle))
+print('The angle is {}, sine {}, cosine{}, and tangent {}'.format(angle, sen, cos, tan))
