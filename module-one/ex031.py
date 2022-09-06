6eb718317ad7d43dc5662de3dcaf8d90311b96cc
Vanessa Furtunato <vanessafurtunato984@gmail.com>
2022-09-05 22:22:30 -0300

    First and last name

---
+++ module-one/ex031.py
@@ -0,0 +1,5 @@
+n = str(input('Type your full name: '))
+name = n.split()
+print('Nice to meet you!')
+print('Your first name is: {}'.format(name[0]))
+print('Your last name is: {}'.format(name[len(name)-1]))
