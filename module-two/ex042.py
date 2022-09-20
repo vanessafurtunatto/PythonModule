712d30a5650a8d909232e78bd13529d62d74b852
Vanessa Furtunato <vanessafurtunato984@gmail.com>
2022-09-19 22:44:09 -0300

    Conversion base

--- module-two/ex042.py
+++ module-two/ex042.py
@@ -5,10 +5,10 @@ print(''' Choose a base for conversion:
  [ 3 ] For Hexa ''')
 options = int(input('Your option: '))
 if options == 1:
-    print('{} converted to binary is {}'.format(num, bin(num)))
+    print('{} converted to binary is {}'.format(num, bin(num)[2:]))
 elif options == 2:
-    print('{} converted to octal is {}'.format(num, oct(num)))
+    print('{} converted to octal is {}'.format(num, oct(num)[2:]))
 elif options == 3:
-    print('{} converted to hexa is {}'.format(num, hex(num)))
+    print('{} converted to hexa is {}'.format(num, hex(num)[2:]))
 else:
     print('() is option invalid, try again.'. format(num))
