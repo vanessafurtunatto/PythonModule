ea7f46bcebeab5b2a839cbb1caa233ab8813887a
Vanessa Furtunato <vanessafurtunato984@gmail.com>
2022-09-20 22:24:22 -0300

    Comparing numbers

---
+++ module-two/ex043.py
@@ -0,0 +1,9 @@
+num1 = int(input('Type the first number: '))
+num2 = int(input('Type the second number: '))
+if num1 > num2:
+    print('The first is bigger')
+elif num2 > num1:
+    print('The second is bigger')
+else:
+    print('Same value')
+
